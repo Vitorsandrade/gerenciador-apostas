0f6d19c4d2f7cca7673a9588ea2c341f188a5fb9
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-07-26 14:32:33 -0300

    First commit

---
+++ src/main/java/com/codex/aposta/GerApostasApplication.java
@@ -0,0 +1,13 @@
+package com.codex.aposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerApostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerApostasApplication.class, args);
+	}
+
+}
